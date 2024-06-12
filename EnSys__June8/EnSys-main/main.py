12ead20cadec407783d9d285017d6163502eff89
yannahlb <75607258+yannahlb@users.noreply.github.com>
2024-06-12 13:33:45 +0800

    Update main.py

--- EnSys__June8/EnSys-main/main.py
+++ EnSys__June8/EnSys-main/main.py
@@ -29,4 +29,5 @@ app = Flask(__name__)
 initialize_routes(app, bot)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
